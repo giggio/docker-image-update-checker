7ff3d5bddd9a5f039a2802fc866dd5b785ac2e77
Giovanni Bassi <giggio@giggio.net>
2022-02-16 03:09:13 -0300

    Work with other registries and Windows
    Also several other updates.

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
 # Docker Image Update Checker Action
 
-[![Test](https://github.com/lucacome/docker-image-update-checker/actions/workflows/test.yml/badge.svg)](https://github.com/lucacome/docker-image-update-checker/actions/workflows/test.yml)
-[![GitHub release badge](https://badgen.net/github/release/lucacome/docker-image-update-checker/stable)](https://github.com/lucacome/docker-image-update-checker/releases/latest)
-[![GitHub license badge](https://badgen.net/github/license/lucacome/docker-image-update-checker)](https://github.com/lucacome/docker-image-update-checker/blob/main/LICENSE)
-[![GitHub Workflows badge](https://badgen.net/runkit/lucacome/lucacome-workflow)](https://github.com/search?q=docker-image-update-checker+path%3A.github%2Fworkflows%2F+language%3AYAML&type=Code)
+[![Test](https://github.com/giggio/docker-image-update-checker/actions/workflows/test.yml/badge.svg)](https://github.com/giggio/docker-image-update-checker/actions/workflows/test.yml)
+[![GitHub release badge](https://badgen.net/github/release/giggio/docker-image-update-checker/stable)](https://github.com/giggio/docker-image-update-checker/releases/latest)
+[![GitHub license badge](https://badgen.net/github/license/giggio/docker-image-update-checker)](https://github.com/giggio/docker-image-update-checker/blob/main/LICENSE)
 
 Action to check if the base image was updated and your image (published on DockerHub) needs to be rebuilt. This action will use Docker's API to compare the base layers of your image with the `base-image`, without the need to pull the images.
 
 
 ## Inputs
 
-| Name                | Type     | Description                        |
-|---------------------|----------|------------------------------------|
-| `base-image`        | String   | Base Docker Image                  |
-| `image`             | String   | Your image based on `base-image`   |
+| Name         | Type   | Description                                                                                                                  |
+| ------------ | ------ | ---------------------------------------------------------------------------------------------------------------------------- |
+| `base-image` | String | Base Docker Image                                                                                                            |
+| `image`      | String | Your image based on `base-image`                                                                                             |
+| `os`         | String | Operating system, necessary if you are using multi arch images and the OS is different from the OS used in the Github action |
+| `verbose`    | bool   | Show verbose output                                                                                                          |
 
-Note: the `base-image` needs to have the full path. For example for official images like `nginx`, the full path is `library/nginx`.
+Note: the `base-image` needs to have the full path.
+
+If you use a simple image name, like `nginx`, it will be translated to the default Docker Hub library images as `index.docker.io/library/nginx`.
+If you use an image with an owner, like `foo/bar`, it will be translated to a Docker Hub images as `index.docker.io/foo/bar`.
+
+The tag is optional, but you could supply it. It will default to `latest`.
+You can use it like `index.docker.io/library/nginx:latest` (full name) or `nginx:latest`.
 
 ## Output
 
-| Name            | Type    | Description                                               |
-|-----------------|---------|-----------------------------------------------------------|
-| `needs-updating`| String  | 'true' or 'false' if the image needs to be updated or not |
+| Name             | Type   | Description                                               |
+| ---------------- | ------ | --------------------------------------------------------- |
+| `needs-updating` | String | `true` or `false` if the image needs to be updated or not |
 
 
 ## Example
@@ -37,18 +44,17 @@ jobs:
   docker:
     runs-on: ubuntu-latest
     steps:
-      -
-        name: Checkout
+      - name: Checkout
         uses: actions/checkout@v2
-      -
-        name: Check if update available
+      - name: Check if update available
         id: check
-        uses: lucacome/docker-image-update-checker@v1
+        uses: giggio/docker-image-update-checker@v2
         with:
           base-image: library/nginx:1.21.0
           image: user/app:latest
-      -
-        name: Build and push
+          os: linux # optional
+          verbose: true # optional
+      - name: Build and push
         uses: docker/build-push-action@v2
         with:
           context: .
--- docker.sh
+++ docker.sh
@@ -1,23 +1,120 @@
 #!/usr/bin/env bash
 
-get_layers() {
-    local repo=$1
-    local digest=$2
+getLayers() {
+    local REGISTRY=$1
+    local REPO=$2
+    local DIGEST_LIST=$3
+    local OS=$4
+    local ARCH=$5
 
-    digestOutput=$(curl -H "Authorization: Bearer $(get_token $repo)" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "https://index.docker.io/v2/${repo}/manifests/${digest}" 2>/dev/null)
-
-    jq -r '[.layers[].digest]' <<<"$digestOutput"
+    local AUTH=`getToken $REGISTRY $REPO`
+    if [ "$AUTH" != "" ]; then
+        AUTH="Authorization: Bearer $AUTH"
+    fi
+    MANIFESTS=`curl -H "$AUTH" -H "Accept: application/vnd.docker.distribution.manifest.list.v2+json" "https://$REGISTRY/v2/$REPO/manifests/$DIGEST_LIST" 2>/dev/null`
+    VERSION=`jq -r .schemaVersion <<< $MANIFESTS`
+    echo 1, $VERSION > /dev/tty
+    if [ "$VERSION" == '1' ]; then
+        jq -r '.fsLayers[] | .blobSum' <<< $MANIFESTS | tac
+    elif [ "$VERSION" == '2' ]; then
+        if [ "$OS" == "" ]; then
+            if [ `uname` == 'Linux' ]; then
+                OS='linux'
+            else
+                OS='windows'
+            fi
+        fi
+        if [ "$ARCH" == "" ]; then
+            case `uname -m` in
+                # todo: handle other cases: ppc64le, s390x, riscv64 etc.
+                # todo: is next line correct?
+                i386)   ARCH="386" ;;
+                # todo: is next line correct?
+                i686)   ARCH="386" ;;
+                x86_64) ARCH="amd64" ;;
+                arm)    dpkg --print-architecture | grep -q arm64 && ARCH=arm64 || ARCH=arm ;;
+                *)
+                    >&2 echo "Unknown architecture: `uname -m`"
+                    exit 1 ;;
+            esac
+        fi
+        export OS=$OS
+        export ARCH=$ARCH
+        DIGEST=`jq -r '[ .manifests[] | select( .platform.architecture == env.ARCH and .platform.os == env.OS ) ][0].digest' <<< $MANIFESTS`
+        if [ "$DIGEST" == "null" ]; then
+            >&2 echo "No manifest found for $OS/$ARCH"
+            exit 1
+        fi
+        MANIFEST=`curl -H "$AUTH" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "https://$REGISTRY/v2/$REPO/manifests/$DIGEST" 2>/dev/null`
+        jq -r '.layers[].digest' <<<"$MANIFEST"
+    else
+        >&2 echo "Unknown schema version: $VERSION"
+        exit 1
+    fi
 }
 
-get_token() {
-    local repo=$1
-    echo $(curl 'https://auth.docker.io/token?service=registry.docker.io&scope=repository:'${repo}':pull' 2>/dev/null | jq -r '.token')
+getToken() {
+    local REGISTRY=$1
+    local REPO=$2
+
+    if [ "$REGISTRY" == "index.docker.io" ]; then
+        curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$REPO:pull" 2>/dev/null | jq -r '.token'
+    else
+        echo ""
+    fi
 }
 
-IFS=: read base base_tag <<<$base
-IFS=: read image image_tag <<<$image
+case `awk -F/ '{print NF-1}' <<< "$FULL_BASE"` in
+    0) FULL_BASE="index.docker.io/library/$FULL_BASE" ;;
+    1)
+        if ! grep -P '(?=^.{4,253}$)(^(?:[a-zA-Z0-9](?:(?:[a-zA-Z0-9\-]){0,61}[a-zA-Z0-9])?\.)+([a-zA-Z]{2,}|xn--[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])$)' <<< "${FULL_BASE%%/*}" > /dev/null; then
+            FULL_BASE="index.docker.io/$FULL_BASE"
+        fi ;;
+esac
+case `awk -F/ '{print NF-1}' <<< "$FULL_IMAGE"` in
+    0) FULL_IMAGE="index.docker.io/library/$FULL_IMAGE" ;;
+    1)
+        if ! grep -P '(?=^.{4,253}$)(^(?:[a-zA-Z0-9](?:(?:[a-zA-Z0-9\-]){0,61}[a-zA-Z0-9])?\.)+([a-zA-Z]{2,}|xn--[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])$)' <<< "${FULL_IMAGE%%/*}" > /dev/null; then
+            FULL_IMAGE="index.docker.io/$FULL_IMAGE"
+        fi ;;
+esac
 
-layers_base=$(get_layers $base ${base_tag:-latest})
-layers_image=$(get_layers $image ${image_tag:-latest})
+BASE_REPO="${FULL_BASE%%/*}"
+IMAGE_REPO="${FULL_IMAGE%%/*}"
+IFS=: read BASE BASE_TAG <<< ${FULL_BASE#*/}
+IFS=: read IMAGE IMAGE_TAG <<< ${FULL_IMAGE#*/}
 
-jq '.base-.image | .!=[]' <<<"{\"base\": $layers_base, \"image\": $layers_image }"
+LAYERS_BASE=`getLayers $BASE_REPO $BASE ${BASE_TAG:-latest} $OS $ARCH`
+if [ "$?" != "0" ]; then
+    >&2 echo "Error getting layers for $FULL_BASE"
+    exit 1
+fi
+if [ "$LAYERS_BASE" == "" ]; then
+    >&2 echo "No layers found for $FULL_BASE"
+    exit 1
+fi
+if [ "$VERBOSE" == "true" ]; then
+    echo Base image: $FULL_BASE
+    echo Layers:
+    echo "$LAYERS_BASE"
+fi
+LAYERS_IMAGE=`getLayers $IMAGE_REPO $IMAGE ${IMAGE_TAG:-latest} $OS $ARCH`
+if [ "$LAYERS_IMAGE" == "" ]; then
+    echo "No layers found for $FULL_IMAGE"
+    exit 1
+fi
+if [ "$?" != "0" ]; then
+    >&2 echo "Error getting layers for $FULL_IMAGE"
+    exit 1
+fi
+if [ "$VERBOSE" == "true" ]; then
+    echo Image: $FULL_IMAGE
+    echo Layers:
+    echo "$LAYERS_IMAGE"
+fi
+
+if [[ "$LAYERS_IMAGE" == "$LAYERS_BASE"* ]]; then
+  echo false
+else
+  echo true
+fi
